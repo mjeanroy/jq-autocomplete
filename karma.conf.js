105301742f139f5ba0f1e40cc174ea4ab530f303
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2013-09-29 22:00:20 +0200

    test: set capture timeout of karma to 10s

--- karma.conf.js
+++ karma.conf.js
@@ -77,7 +77,7 @@ module.exports = function(config) {
 
     // If browser does not capture in given timeout [ms], kill it
     // CLI --capture-timeout 5000
-    captureTimeout: 5000,
+    captureTimeout: 10000,
 
     // Auto run tests on start (when browsers are captured) and exit
     // CLI --single-run --no-single-run
